266946b202542d80b32756e37029adbd0e012f44
sbahrami <sinabah@gmail.com>
2023-02-12 10:55:27 -0500

    --all

---
+++ asgn1_lib.R
@@ -0,0 +1,30 @@
+detect_outlier <- function(x) {
+  
+  # calculate first quantile
+  Quantile1 <- quantile(x, probs=.25, na.rm = TRUE)
+  
+  # calculate third quantile
+  Quantile3 <- quantile(x, probs=.75, na.rm = TRUE)
+  
+  # calculate inter quartile range
+  IQR = Quantile3-Quantile1
+  
+  # return true or false
+  x > Quantile3 + (IQR*1.5) | x < Quantile1 - (IQR*1.5)
+}
+
+# create remove outlier function
+remove_outlier <- function(dataframe,
+                           columns=names(dataframe)) {
+  
+  # for loop to traverse in columns vector
+  for (col in columns) {
+    
+    # remove observation if it satisfies outlier function
+    dataframe <- dataframe[!detect_outlier(dataframe[[col]]), ]
+  }
+  
+  # return dataframe
+  print("Remove outliers")
+  print(dataframe)
+}
